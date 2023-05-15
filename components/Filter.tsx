e3cb3710395a1202d27be344526c5082b971edf1
jbxamora <jbxamora@icloud.com>
2023-05-14 20:05:16 -0700

    revert - filter handler function

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -3,31 +3,25 @@ import axios from "axios";
 
 type FilterComponentProps = {
   categories: string[];
-  onCategorySelect: (category: string) => void;
+  onCategorySelect: (category: string) => void; 
 };
 
+const FilterComponent: React.FC<FilterComponentProps> = ({ categories, onCategorySelect }) => {
+  // Rest of the component
 
-
-const FilterComponent: React.FC<FilterComponentProps> = ({
-  categories,
-  onCategorySelect,
-}) => {
-  
   const [images, setImages] = useState([]);
 
   const handleCategorySelect = async (category) => {
     const res = await axios.get(`/api/images?folder=${category}`);
     setImages(res.data.images);
-    onCategorySelect(category);
   };
 
-
   return (
-    <div className="flex flex-wrap justify-around gap-4 rounded bg-white/10 px-4 py-4 sm:px-0">
+    <div className="bg-white/10 rounded gap-4 flex flex-wrap justify-around py-4 px-4 sm:px-0">
       {categories.map((category, index) => (
         <button
           key={index}
-          className="m-1 rounded-lg border px-2 py-1 text-xs text-gray-200 shadow-md transition-colors duration-200 hover:animate-pulse focus:outline-none sm:m-0 sm:px-4 sm:py-2 sm:text-base"
+          className="rounded-lg border px-2 py-1 text-xs text-gray-200 shadow-md transition-colors duration-200 hover:animate-pulse focus:outline-none m-1 sm:m-0 sm:px-4 sm:py-2 sm:text-base"
           onClick={() => handleCategorySelect(category)}
         >
           {category}
