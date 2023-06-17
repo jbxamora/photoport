a33bb233f4ee7c71c9861434864130093bc403d9
jbxamora <jbxamora@icloud.com>
2023-06-17 12:40:34 -0700

    vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import type { AppProps } from 'next/app'
-import '../styles/index.css'
+import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
+import "../styles/index.css";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
+
+export default MyApp;
+
